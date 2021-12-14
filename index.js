63fac2292c5a54293d92fbf51b4f61da46eddb6b
Sebastian Andreasson <xandse@MacBook-Pro-som-tillhor-Sebastian.local>
2021-12-14 16:24:03 +0100

    comment ssh command

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.get('/ping', (req, res) => res.send('pong'))
+app.get('/ping', (req, res) => {
+    console.log('ping')
+    res.send('pong')
+})
 
 app.listen(3000, () => console.log('listening on port 3000'))
+
+// ssh -i ./appademin-access-point.pem -R 8080:localhost:3000 ec2-user@ec2-16-171-22-69.eu-north-1.compute.amazonaws.com
